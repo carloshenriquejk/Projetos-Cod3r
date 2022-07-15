008480486822ae4be386fd19023ff094cf7b24bb
Carlos Henrique <105329872+carloshenriquejk@users.noreply.github.com>
2022-07-15 19:06:33 -0300

    Add files via upload

---
+++ componentes/src/componentes/BomDia.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default props => 
+  <div>
+  <h1>bom dia {props.nome}!</h1>
+  <h2>até breve!</h2>
+  </div>
+
---
+++ componentes/src/index.js
@@ -0,0 +1,7 @@
+ import React  from "react"
+ import ReactDOM  from "react-dom"
+ import Primeiro from './componentes/primeiro'
+ import BomDia from "./componentes/BomDia";
+const elemento = <h1>React</h1>;
+ //ReactDOM.render(<Primeiro/>, document.getElementById('root'))
+ ReactDOM.render(<BomDia nome ="Guilherme"/>, document.getElementById('root'))
